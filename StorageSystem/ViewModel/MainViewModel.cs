e4583565419223ae85f61b7cc45f60726b0fd4d0
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-03-03 22:53:18 +0300

    Добавлен Mediator для передачи данных между страницами

--- StorageSystem/ViewModel/MainViewModel.cs
+++ StorageSystem/ViewModel/MainViewModel.cs
@@ -1,54 +1,11 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Windows;
 
 namespace StorageSystem.ViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-
-
-        private string _login;
-        public string Login
-        {
-            get
-            {
-                return _login;
-            }
-            set
-            {
-                _login = value;
-                OnPropertyChanged();
-            }
-        }
-        private string _password;
-        public string Password
-        {
-            get
-            {
-                return _password;
-            }
-            set
-            {
-                _password = value;
-                OnPropertyChanged();
-            }
-        }
-        private bool _rememberMe;
-        public bool RememberMe
-        {
-            get
-            {
-                return _rememberMe;
-            }
-            set
-            {
-                _rememberMe = value;
-                OnPropertyChanged();
-            }
-        }
-        private string _pageSours = "Authorization.xaml";
+        private string _pageSours;
         public string PageSours
         {
             get
@@ -61,51 +18,15 @@ namespace StorageSystem.ViewModel
                 OnPropertyChanged();
             }
         }
-        public DelegateCommand send
-        {
-            get
-            {
-                return new DelegateCommand((obj) =>
-                {
-                    /*
-                    if(Login.Length < 5 || Password.Length < 5)
-                    {
-                        MessageBox.Show("нет");
-                    }
-                    else
-                    {
-                        
-                        if (RememberMe)
-                        {
-                            MessageBox.Show("Настройки сохранены");
-                            SetSettings(Login, Password, RememberMe);
-                        }
-                        else
-                        {
-                            SetSettings("","",false);
-                        }
-                        
-                    }*/
-                    PageSours = "MainUI.xaml";
-                });
-            }
-        }
         public MainViewModel()
         {
-            GetSettings();
+            Mediator.Instance.GoToMainUI += OnMessageReceived;
+            PageSours = "Authorization.xaml";
         }
-        private void SetSettings(string Login, string Password, bool RememberMe)
+        private void OnMessageReceived(string receiver, string message)
         {
-            Properties.Settings.Default.Login = Login;
-            Properties.Settings.Default.Password = Password;
-            Properties.Settings.Default.RememberMe = RememberMe;
-            Properties.Settings.Default.Save();
-        }
-        private void GetSettings()
-        {
-            Login = Properties.Settings.Default.Login;
-            Password = Properties.Settings.Default.Password;
-            RememberMe = Properties.Settings.Default.RememberMe;
+            if(receiver == "MainForm")
+                PageSours = message;
         }
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string name = "")
---
+++ StorageSystem/ViewModel/Mediator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Mediator
+{
+    private static Mediator instance;
+    private Mediator() { }
+
+    public static Mediator Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new Mediator();
+            return instance;
+        }
+    }
+
+    public event Action<string, string> GoToMainUI;
+
+    public void SendMessage(string receiver, string message)
+        => GoToMainUI?.Invoke(receiver, message);
+}
